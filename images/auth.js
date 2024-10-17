036eee638e42a8d0957618d0e90af5636d2479b1
拉胯人 <52907859+TrashManOfficial@users.noreply.github.com>
2024-10-17 16:19:48 +0800

    feat:权限对接

---
+++ images/auth.js
@@ -0,0 +1,46 @@
+function getAuth() {
+  var siteid = document.head.getElementsByTagName('meta')['siteid'].getAttribute('content')
+  var channelid = document.head.getElementsByTagName('meta')['channelid'].getAttribute('content')
+  trsAxios
+    .get("/wcm/mlfcenter.do", {
+      params: {
+        methodname: "checkWebSiteRight",
+        serviceid: "mlf_mediasite",
+        siteId: siteid,
+        channelId: channelid
+      },
+    })
+    .then((res) => {
+      if (res.HASRIGHT === 'false') {
+            $('body').html('<div style="width: 100%;text-align: center; font-size: large;margin-top: 20px">暂无查看权限</div>')
+            return
+      }
+      $(".dw_select span").text(res.SITENAME)
+      getSiteData()
+      // console.log(res);
+    });
+}
+function setDropDownList(list) {
+  $.each(list, function (index, item) {
+    $(".dw_select ul").append(
+      $("<li>", { value: item.ROOTDOMAIN, text: item.NAME }).on(
+        "click",
+        function () {
+          location.href = $(this).attr("value")
+        }
+      )
+    );
+  });
+}
+function getSiteData() {
+  trsAxios
+    .get(
+      "/wcm/mlfcenter.do?methodname=queryPortal&serviceid=mlf_mediasite"
+    )
+    .then((res) => {
+      setDropDownList(res);
+    });
+}
+window.onload = function () {
+  getAuth();
+};
